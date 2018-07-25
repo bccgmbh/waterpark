9d1c3888c5291b070830f30c7002794e6109d4c9
Marc Ablay <marc.ablay@blockchain-consulting.net>
2018-07-25 08:50:31 +0200

    removed last external dependency! :sunny:

--- src/transform/multicore-transform.js
+++ src/transform/multicore-transform.js
@@ -1,35 +1,28 @@
 const {fork} = require('child_process')
-const parallel = require('parallel-transform')
-// const debug = require('debug')('multicore')
+const {concurrent} = require('./concurrent-transform')
 
-const multicore = (path, cores = 1) => {
+module.exports.multicore = (path, cores = 1, options = {}) => {
   const children = []
 
-  // debug('with %d cores', cores)
   for (let i = 0; i < cores; i++) {
     children.push(fork(path))
-    // debug('Forked sub process %d', children[i].pid)
   }
 
-  const transform = parallel(
-    cores,
-    {
-      // highWaterMark: cores,
-      objectMode: true
-    },
-    function (data, cb) {
+  const transform = concurrent({
+    ...options,
+    objectMode: true,
+    concurrency: cores,
+    transform: function multicoreTransform (data, encoding, cb) {
       const idleChild = children.find((child) => !child.hasOwnProperty('cb'))
       if (!idleChild) throw new Error('No idle idleChild available!')
-
-      // debug('%s => %d', data.padStart(3), idleChild.pid)
       idleChild.cb = cb
       idleChild.send(data)
     }
-  )
+
+  })
 
   children.forEach((child, index) => {
     child.on('message', (msg) => {
-      // debug('%s <= %d | %s', msg[0].padStart(3), child.pid, msg[1])
       const cb = child.cb
       delete child.cb
       cb(null, msg)
@@ -41,8 +34,4 @@ const multicore = (path, cores = 1) => {
   })
 
   return transform
-}
-
-module.exports = {
-  multicore
 }
