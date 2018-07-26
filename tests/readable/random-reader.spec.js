047a0af9412bfb6cd6641d8b5f7f54e06eb8c249
Marc Ablay <marc.ablay@blockchain-consulting.net>
2018-07-26 20:55:56 +0200

    Revert "[comments] illustrated slice"
    This reverts commit 41aba48c31e0c54be5a172b4b66edf88aba26418.

--- tests/readable/random-reader.spec.js
+++ tests/readable/random-reader.spec.js
@@ -1,29 +1,17 @@
 const tape = require('tape')
 const {random} = require('../../')
+// const debug = require('debug')('wp-spec-reader-random')
 
-// const DEFAULT_HIGH_WATERMARK = 16384
+const DEFAULT_HIGH_WATERMARK = 16384
 
-tape('[RandomReader] default random number stream', t => {
-  let count = 10
-  t.plan(count)
-  const rr = random()
-    .on('data', data => {
-      t.equal(typeof data, 'number', 'random number ' + data)
-      if (--count < 1) {
-        rr.pause()
-        rr.destroy()
-      }
-    })
-})
-
-tape('[RandomReader] default random buffer stream', t => {
+tape('[RandomReader] Random stream (default: infinite buffer)', t => {
   let count = 3
   t.plan(count * 2)
-  const HIGH_WATERMARK = 8
-  const rr = random.buf({highWaterMark: HIGH_WATERMARK})
+  const rr = random()
     .on('data', data => {
-      t.ok(Buffer.isBuffer(data), 'buffer emitted')
-      t.equal(data.length, HIGH_WATERMARK, 'Random buffer should have specified highWatermark ' + data.toString('hex'))
+      // console.log('[TEST:RANDOM] buffer length %s', data.toString('hex'))
+      t.ok(Buffer.isBuffer(data), 'random buffer produced')
+      t.equal(data.length, DEFAULT_HIGH_WATERMARK, 'Random buffer should have default highWatermark')
       if (--count < 1) {
         rr.pause()
         rr.destroy()
@@ -31,17 +19,69 @@ tape('[RandomReader] default random buffer stream', t => {
     })
 })
 
-tape('[RandomReader] hex encoded random stream', t => {
+tape('[RandomReader] infinite buffer stream with highWaterMark', t => {
+  let count = 3
+  t.plan(count * 2)
+  const HIGH_WATERMARK = 20
+  const rr = random(null, null, {highWaterMark: HIGH_WATERMARK})
+    .on('data', data => {
+      t.ok(Buffer.isBuffer(data), 'random buffer produced')
+      t.equal(data.length, HIGH_WATERMARK, 'Random buffer should have specified highWatermark')
+      if (--count < 1) {
+        rr.pause()
+        rr.destroy()
+      }
+    })
+})
+
+tape('[RandomReader] infinite hex encoded stream', t => {
   let count = 3
   t.plan(count * 2)
   const highWaterMark = 20
-  const rr = random.buf({highWaterMark, encoding: 'hex'})
-    .on('data', data => {
-      t.equal(data.length, highWaterMark * 2, 'Random buffer length equals highWatermark')
-      t.equal(typeof data, 'string', 'Type of data should be string')
-      if (--count < 1) {
-        rr.pause()
-        rr.destroy()
-      }
-    })
+  const rr = random(null, 'hex', {objectMode: true, highWaterMark}).on('data', data => {
+    t.equal(data.length, highWaterMark * 2, 'Random buffer should have specified highWatermark')
+    t.equal(typeof data, 'string', 'Type of data should be string')
+    if (--count < 1) {
+      rr.pause()
+      rr.destroy()
+    }
+  })
+})
+
+tape('[RandomReader] finite buffer stream', t => {
+  let count = 0
+  const LENGTH = 20000
+  random(LENGTH).on('data', data => {
+    t.ok(Buffer.isBuffer(data), 'Data should be a buffer')
+    count += data.length
+    // debug('[Test.FiniteBuffer] count %d', count)
+  }).on('end', () => {
+    t.equal(count, LENGTH, 'Total bytes should equal specified length')
+    t.end()
+  })
+})
+
+tape('[RandomReader] finite hex stream', t => {
+  let count = 0
+  const LENGTH = 20
+  random(LENGTH, 'hex').on('data', data => {
+    t.ok((typeof data === 'string'), 'Data should be a string')
+    count += data.length
+  }).on('end', () => {
+    // hex string requires 2 characters to encode one byte
+    t.equal(count, LENGTH * 2, 'Total bytes should equal specified length')
+    t.end()
+  })
+})
+
+tape('[RandomReader] finite ascii stream with options', t => {
+  let count = 0
+  const LENGTH = 20
+  random(LENGTH, 'ascii', {highWaterMark: 1000}).on('data', data => {
+    t.ok((typeof data === 'string'), 'Data should be a string')
+    count += data.length
+  }).on('end', () => {
+    t.equal(count, LENGTH, 'Total bytes should equal specified length')
+    t.end()
+  })
 })
