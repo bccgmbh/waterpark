efa591428d4e350e7441db3b7aca1f7f7b883d11
Marc Ablay <marc.ablay@blockchain-consulting.net>
2018-07-27 11:09:03 +0200

    code cleanup

--- src/transform/take-transform.js
+++ src/transform/take-transform.js
@@ -1,3 +1,4 @@
 const {slice} = require('./slice-transform')
+
 module.exports.take = (amount, every, options = {}) => slice({end: amount, every, ...options})
 module.exports.take.obj = (amount, every, options = {}) => slice.obj({end: amount, every, ...options})
