0857375c0111039d1c7079500efe9cdaf7ac9c4d
Marc Ablay <marc.ablay@blockchain-consulting.net>
2018-07-27 22:18:54 +0200

    [spec] added interval-reader buffer mode specs

--- tests/readable/interval-reader.spec.js
+++ tests/readable/interval-reader.spec.js
@@ -4,9 +4,23 @@ const {interval, take} = require('../../')
 tape('[IntervalReader] interval(100).pipe(take(3)) should porperly terminate around 300ms', t => {
   t.plan(4)
   const startTime = Date.now()
-  const ir = interval(100)
+  const ir = interval(100, {objectMode: true})
   ir.pipe(take.obj(3))
     .on('data', (data) => t.ok(data, `interval emitted ${Date.now() - startTime}`))
+    .on('end', () => {
+      ir.destroy()
+      const endTime = Date.now()
+      const duration = endTime - startTime
+      t.ok(duration > 270 && duration < 330, 'interval for 3 x 100ms took ' + duration + 'ms')
+    })
+})
+
+tape('[IntervalReader] should support buffer mode', t => {
+  t.plan(4)
+  const startTime = Date.now()
+  const ir = interval(100, {objectMode: false})
+  ir.pipe(take(13 * 3))
+    .on('data', (data) => t.ok(data, `buffer emitted ${data}`))
     .on('end', () => {
       ir.destroy()
       const endTime = Date.now()
